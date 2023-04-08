474389ad10cb67bf1980e51c4d7f7d23fb0b5baf
Gopala <guptaraja727@gmail.com>
2023-04-08 13:37:11 +0530

    first commit

---
+++ server/database/db.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const Connection = () => {
+
+    const MONGODB_URI = `mongodb://127.0.0.1:27017/todo`;
+
+
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    })
+}
+
+export default Connection;
